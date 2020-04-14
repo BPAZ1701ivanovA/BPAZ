8e8e61bf31b4704665a6d42e849739ad9be49d56
Andrey ivanov <andreyivanov-2010@mail.ru>
2020-04-15 02:36:54 +0700

    Deleted unsuded files, completed labout work number one.

--- 1/Solution_star.java
+++ 1/Solution_star.java
@@ -1,24 +1,21 @@
-import java.util.*;
-
-public class Solution {
-	/*
-	*  Дано n - число уровней. Построить треугольник из символов #.
-	*  Пример для n = 4:
-	*	   #	
-	*	  ##
-	*	 ###
-	*	####
-	*/	 
-	    
-	static void func1(int n) {
-     
-       }
-    
-	private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        func1(n);
-	}	
+//Моя IDE (Eclipse) пожелала, чтобы я переименовал этот класс.
+public class Solution_star
+{
+	//Так как мой вариант = 15, то нет смысла парсить ввод пользователя.
+	public static void main(String[] args)
+	{
+	    int n = 15;
+	    for (int i = 1; i <= n; i++)
+	    {
+	    	for(int j = n; j >= i; j--)
+	    	{
+	    		System.out.print(" ");
+	    	}
+	        for (int k = i; k > 0; k--)
+	        {
+	        	System.out.print("#");
+	        }  
+	    System.out.println();
+	    }
+	}
+}
